5f5c2b1c5bc2c77940ff60c2f296f49118376966
Jarrod Alexander <jarrod.alexander@gmail.com>
2017-03-28 16:39:23 -0500

    Correcting issue with how `EventListeners` are registered with the IoC container.

--- src/TwentyTwenty.MessageBus.Providers.MassTransit/ConsumerFactories/EventListenerConsumerFactory.cs
+++ src/TwentyTwenty.MessageBus.Providers.MassTransit/ConsumerFactories/EventListenerConsumerFactory.cs
@@ -5,14 +5,15 @@ using TwentyTwenty.DomainDriven;
 
 namespace TwentyTwenty.MessageBus.Providers.MassTransit.ConsumerFactories
 {
-    public class EventListenerConsumerFactory<TMessage> : ConsumerFactory<MassTransitConsumer<TMessage>>
-        where TMessage : class, IDomainEvent        
+    public class EventListenerConsumerFactory<TMessage, TListener> : ConsumerFactory<MassTransitConsumer<TMessage>>
+        where TMessage : class, IDomainEvent
+        where TListener : class, IHandle<TMessage>
     {
         public EventListenerConsumerFactory(IServiceProvider services) : base(services) { }
 
         protected override MassTransitConsumer<TMessage> GetConsumer(IServiceProvider services)
         {
-            var handler = services.GetService<IEventListener<TMessage>>();
+            var handler = services.GetService<TListener>();
 
             if (handler == null)
             {
